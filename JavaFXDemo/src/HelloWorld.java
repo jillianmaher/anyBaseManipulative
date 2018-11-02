38f7f3a69b87021a9ee1984c8656f1e79dfc5ba9
Jillian Maher <jillian.maher2@boone.kyschools.us>
2018-11-02 10:42:30 -0400

    Testing opening local Java Project from Git

--- JavaFXDemo/src/HelloWorld.java
+++ JavaFXDemo/src/HelloWorld.java
@@ -14,9 +14,11 @@ public class HelloWorld extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        Button btn1 = new Button();
+        btn1.setText("Say 'Hello World'");
+        Button btn2 = new Button();
+        btn2.setText("Say 'QUIT'");
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
             public void handle(ActionEvent event) {
@@ -24,8 +26,18 @@ public class HelloWorld extends Application {
             }
         });
         
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+        	 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("QUIT");
+            }
+        });
+
+        
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        root.getChildren().add(btn1);
+        root.getChildren().add(btn2);
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
     }
